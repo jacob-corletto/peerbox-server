d01784b2814976f543ff1b9cbc0186bed915b419
jacob-corletto <jacob.corletto@csu.fullerton.edu>
2024-11-27 17:20:13 -0800

    super brief outline

--- index.js
+++ index.js
@@ -4,34 +4,34 @@
 // Entry point for PeerBox Server
 // Runs on port 8888
 //=================================================================
-require('dotenv').config(); // Load Environment Variables from .env
-const express = require('express');
-const mysql = require('mysql2/promise');
-const helmet = require('helmet');
-const cors = require('cors');
+require("dotenv").config(); // Load Environment Variables from .env
+const express = require("express");
+const mysql = require("mysql2/promise");
+const helmet = require("helmet");
+const cors = require("cors");
 
 // Import routes
 
 // Server Configs
 const app = express();
-const HOST = 'localhost';
+const HOST = "localhost";
 const PORT = process.env.PORT || 8888; // Server port
 let db;
 
 // Setup database connection
-(async() => {
-	try {
-		db = await mysql.createConnection({
-			host: process.env.DB_HOST,
-			user: process.env.DB_USER,
-			password: process.env.DB_PASSWORD,
-			database: process.env.DB_NAME
-		});
-		
-		console.log('Connected to Amazon RDS: pb-rds-database');
-	} catch (err) {
-		console.error('Error connecting to database:', err);
-	}
+(async () => {
+  try {
+    db = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+
+    console.log("Connected to Amazon RDS: pb-rds-database");
+  } catch (err) {
+    console.error("Error connecting to database:", err);
+  }
 })();
 
 // Middleware
@@ -41,19 +41,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Starter Endpoint
-app.get('/', (req, res) => {
-	res.json("Welcome to the PeerBox Server!");
-})
+app.get("/", (req, res) => {
+  res.json("Welcome to the PeerBox Server!");
+});
 
 // Routes
+//callback
+
+//users
+
+//files
+
+//file actions (upload, download, delete)
+
+//roles (admin, user)
+
+//access controls
 
 // Start Server
 app.listen(PORT, () => {
-	console.log(`Server is running at http://${HOST}:${PORT}`);
+  console.log(`Server is running at http://${HOST}:${PORT}`);
 });
 
 //module.exports = app;
-
-
-
-
